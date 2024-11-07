6185fc9b1df82a192332ab50823bba0b6a41e47e
BCarmine52 <cagueibosta17@gmail.com>
2024-11-06 22:57:47 -0300

    Create newhud.lua

---
+++ newhud.lua
@@ -0,0 +1,40 @@
+-- Biblioteca UnLeaked modificada para suportar múltiplas tabs
+--[[
+  UI lib made by bungie#0001
+  
+  - Please do not use this without permission, I am working really hard on this UI to make it perfect and do not have a big 
+    problem with other people using it, please just make sure you message me and ask me before using.
+]]
+
+-- [CONTINUAÇÃO DO CÓDIGO ORIGINAL]
+
+local library = {
+    version = "2.0.2",
+    title = title or "xsx " .. tostring(math.random(1,366)),
+    fps = 0,
+    rank = "private"
+}
+
+coroutine.wrap(function()
+    RunService.RenderStepped:Connect(function(v)
+        library.fps =  math.round(1/v)
+    end)
+end)()
+
+-- Função modificada para criação de múltiplas tabs
+function library:Init(key)
+    for _,v in next, CoreGuiService:GetChildren() do
+        if v.Name == "screen" then
+            v:Destroy()
+        end
+    end
+
+    local title = library.title
+    key = key or Enum.KeyCode.RightAlt
+
+    -- [CÓDIGO DA GUI E CRIAÇÃO DE ELEMENTOS DA INTERFACE]
+    -- AQUI SERÁ INCLUÍDA A FUNÇÃO PARA CRIAÇÃO E EXIBIÇÃO DAS MULTIPLAS TABS
+end
+
+-- [RESTANTE DO CÓDIGO ORIGINAL]
+
